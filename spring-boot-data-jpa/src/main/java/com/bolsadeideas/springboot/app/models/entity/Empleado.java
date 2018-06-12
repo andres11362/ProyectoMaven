952c7eeb10c6609d7340ebfcdf301de006f8577a
andres11362 <agongorita@outlook.com>
2018-06-12 14:34:52 -0500

    Añadida clase empleado

---
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/app/models/entity/Empleado.java
@@ -0,0 +1,112 @@
+package com.bolsadeideas.springboot.app.models.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+@Entity
+@Table(name = "empleados")
+public class Empleado implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotEmpty
+	private String nombre;
+	
+	@NotEmpty
+	private String apellidos;
+	
+	@NotEmpty
+	private String telefono;
+	
+	@NotEmpty
+	private String direccion;
+	
+	@NotEmpty
+	@Email
+	private String email;
+
+	public Empleado(Long id, String nombre, String apellidos, String telefono, String direccion, String email) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.email = email;
+	}
+
+	public Empleado() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", telefono=" + telefono
+				+ ", direccion=" + direccion + ", email=" + email + "]";
+	}
+	
+	
+	
+	
+	
+}
